c209a4d4058e5c787b4a81ef07431a2346ffaa89
NoahPannicke <50919321+NoahPannicke@users.noreply.github.com>
2024-09-14 13:21:18 +0200

    Update packages_ma.R

--- packages_ma.R
+++ packages_ma.R
@@ -1,17 +1,5 @@
 ##### This script loads all packages required for the project
 
-# todo manage to ensure package versions for consistency
-
-## r version 3.6.1
-sessionInfo()
-
-## not working yet because of Roracle. does not install from source
-## install.packages("checkpoint")
-## library(checkpoint)
-## checkpoint("2020-08-10")
-## Load packages
-
-#install.packages("ROracle")
 package_list <- c("xgboost","ranger","tcltk","data.table","tictoc","pROC","logging","magrittr","keyring","stringr","lubridate","ggplot2",
                   "PRROC","pROC","dplyr","readr","readxl","readr","readxl","stringr","tibble","tidyr","purrr","forcats","caret","e1071",
                   "Metrics","yardstick","caret","rpart","rpart.plot","scales","ComplexHeatmap","palettes","ggsci")
